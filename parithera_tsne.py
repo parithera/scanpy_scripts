d2216fbc4b3696f4c24c807db110462cb44501ce
Cédric HERZOG <cedric.herzog@codeclarity.io>
2025-02-17 11:55:16 +0100

    Add first scripts

---
+++ parithera_tsne.py
@@ -0,0 +1,18 @@
+
+import scanpy as sc
+import anndata as ad
+import sys
+import hdf5plugin
+import os
+import json
+
+if __name__=='__main__':
+    output_path = sys.argv[1]
+    sc.settings.figdir = output_path
+
+    sc.settings.set_figure_params(dpi=50, facecolor="white")
+    adata = ad.read_h5ad(output_path.replace("python", "out.h5ad"))
+    
+    # Plot t-SNE
+    sc.pl.tsne(adata, color='sample', color_map='PuRd', size=50, save='graph.png')
+
---
+++ parithera_umap.py
@@ -0,0 +1,17 @@
+
+import scanpy as sc
+import anndata as ad
+import sys
+import hdf5plugin
+import os
+import json
+
+if __name__=='__main__':
+    output_path = sys.argv[1]
+    sc.settings.figdir = output_path
+
+    sc.settings.set_figure_params(dpi=50, facecolor="white")
+    adata = ad.read_h5ad(output_path.replace("python", "out.h5ad"))
+    
+    # Generating UMAP plot
+    sc.pl.umap(adata, color='sample', save='graph.png', color_map="PuRd", size=50)
